bfea6e37e68ca77237f4544670810573132547a2
Samuel Powell <samuelpowell@users.noreply.github.com>
2018-08-14 13:50:01 +0100

    Add 32 bit support

--- deps/build.jl
+++ deps/build.jl
@@ -15,13 +15,14 @@ function validate_libFTD2XX_version(name, handle)
     return v[] >= 0x00021228
 end
 
-libFTD2XX = library_dependency("libFTD2XX", aliases = ["ftd2xx64", "libftd2xx"], validate=validate_libFTD2XX_version)
+libFTD2XX = library_dependency("libFTD2XX", aliases = ["ftd2xx64", "ftd2xx", "libftd2xx"], validate=validate_libFTD2XX_version)
 
 libFTD2XX_win_x64_URI = URI("http://www.ftdichip.com/Drivers/CDM/CDM%20v2.12.28%20WHQL%20Certified.zip")
 libFTD2XX_glx_x64_URI = URI("http://www.ftdichip.com/Drivers/D2XX/Linux/libftd2xx-x86_64-1.4.8.gz")
 libFTD2XX_osx_x64_URI = URI("http://www.ftdichip.com/Drivers/D2XX/MacOSX/D2XX1.4.4.dmg")
 
-provides(Binaries, libFTD2XX_win_x64_URI, libFTD2XX, unpacked_dir = ".", installed_libpath = joinpath(@__DIR__, "libFTD2XX", "amd64"), os = :Windows)
+provides(Binaries, libFTD2XX_win_x64_URI, libFTD2XX, unpacked_dir = ".",
+        installed_libpath = joinpath(@__DIR__, "libFTD2XX", Sys.WORD_SIZE == 64 ? "amd64" : "i386"), os = :Windows)
 
 @BinDeps.install Dict(:libFTD2XX => :libFTD2XX)
 
