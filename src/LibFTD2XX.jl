42aa4c450d4e2b969ff7e2bf3ba4e6e80481f7f6
Reuben Hill <ReubenHill@users.noreply.github.com>
2019-03-19 15:32:18 +0000

    add device-wide comms functions and hardware tests for those functions

--- src/LibFTD2XX.jl
+++ src/LibFTD2XX.jl
@@ -59,6 +59,8 @@ mutable struct D2XXDevice <: IO
 end
 
 
+# D2XXDevice Constructors
+#
 """
     D2XXDevice(deviceidx::Integer)
 
@@ -85,6 +87,246 @@ function getdevices()
 end
 
 
+# Port communication functions
+#
+Base.isopen(d::D2XXDevice) = isopen(fthandle(d))
+
+function Base.isopen(handle::FT_HANDLE)
+  open = true
+  if ptr(handle) == C_NULL
+    open = false
+  else
+    try
+      FT_GetModemStatus(handle)
+    catch ex
+      if ex == FT_INVALID_HANDLE
+        open = false
+      else
+        rethrow(ex)
+      end
+    end
+  end
+  open
+end
+
+
+"""
+    Base.open(d::D2XXDevice)
+
+Open a D2XX device of type `D2XXDevice`.
+"""
+function Base.open(d::D2XXDevice)
+  d.fthandle = open(description(d), OPEN_BY_DESCRIPTION)
+  return
+end
+
+"""
+    open(str::AbstractString, openby::FTOpenBy)
+
+Open an FTD2XX device.
+
+# Arguments
+ - `str::AbstractString` : Device identifier. Type depends on `openby`
+ - `openby::FTOpenBy` : Indicator of device identifier `str` type.
+
+# Example
+
+```julia-repl
+julia> numdevs = FT_CreateDeviceInfoList()
+0x00000004
+
+julia> idx, flags, type, id, locid, serialnumber, description, fthandle = FT_GetDeviceInfoDetail(0)
+(0, 0x00000002, 0x00000007, 0x04036011, 0x00000000, "FT3AD2HCD", "USB <-> Serial Converter D", FT_HANDLE(Ptr{Nothing} @0x0000000000000000))
+
+julia> handle = open(description, OPEN_BY_DESCRIPTION)
+FT_HANDLE(Ptr{Nothing} @0x0000000000dfe740)
+
+julia> isopen(handle)
+true
+
+julia> close(handle)
+
+julia> handle = open(serialnumber, OPEN_BY_SERIAL_NUMBER)
+FT_HANDLE(Ptr{Nothing} @0x0000000005448ea0)
+
+julia> isopen(handle)
+true
+
+julia> close(handle)
+```
+"""
+Base.open(str::AbstractString, openby::FTOpenBy) = FT_OpenEx(str, DWORD(openby))
+
+
+Base.close(d::D2XXDevice) = close(fthandle(d))
+
+"""
+    Base.close(handle::FT_HANDLE)
+
+Closes an open FTD2XX device and marks its handle as closed.
+"""
+function Base.close(handle::FT_HANDLE)
+  if isopen(handle)
+    FT_Close(handle)
+  end
+  return
+end
+
+
+Compat.bytesavailable(d::D2XXDevice) = bytesavailable(fthandle(d))
+
+function Compat.bytesavailable(handle::FT_HANDLE)
+  isopen(handle) || throw(D2XXException("Device must be open to check bytes available."))
+  FT_GetQueueStatus(handle)
+end
+
+
+Base.eof(d::D2XXDevice) = eof(fthandle(d))
+
+Base.eof(handle::FT_HANDLE) = (bytesavailable(handle) == 0)
+
+
+Base.readbytes!(d::D2XXDevice, b::AbstractVector{UInt8}, nb=length(b)) =
+readbytes!(fthandle(d), b, nb)
+
+function Base.readbytes!(handle::FT_HANDLE, b::AbstractVector{UInt8}, nb=length(b))
+  isopen(handle) || throw(D2XXException("Device must be open to read."))
+  nbav = bytesavailable(handle)
+  if nbav < nb
+    nb = nbav
+  end
+  if length(b) < nb
+    resize!(b, nb)
+  end
+  nbrx = FT_Read(handle, b, nb)
+end
+
+
+Base.readavailable(d::D2XXDevice) = readavailable(fthandle(d))
+
+function Base.readavailable(handle::FT_HANDLE)
+  b = @compat Vector{UInt8}(undef, bytesavailable(handle))
+  readbytes!(handle, b)
+  b
+end
+
+
+Base.write(d::D2XXDevice, buffer::Vector{UInt8}) = write(fthandle(d), buffer)
+
+function Base.write(handle::FT_HANDLE, buffer::Vector{UInt8})
+  isopen(handle) || throw(D2XXException("Device must be open to write."))
+  FT_Write(handle, buffer, length(buffer))
+end
+
+
+baudrate(d::D2XXDevice, baud) = baudrate(fthandle(d), baud)
+
+function baudrate(handle::FT_HANDLE, baud)
+  isopen(handle) || throw(D2XXException("Device must be open to set baudrate."))
+  FT_SetBaudRate(handle, baud)
+end
+
+
+datacharacteristics(d::D2XXDevice; 
+                    wordlength::FTWordLength = BITS_8, 
+                    stopbits::FTStopBits = STOP_BITS_1, 
+                    parity::FTParity = PARITY_NONE) = 
+datacharacteristics(fthandle(d), 
+                    wordlength=wordlength, 
+                    stopbits=stopbits, 
+                    parity=parity)
+
+function datacharacteristics(handle::FT_HANDLE; 
+                             wordlength::FTWordLength = BITS_8, 
+                             stopbits::FTStopBits = STOP_BITS_1, 
+                             parity::FTParity = PARITY_NONE)
+isopen(handle) || throw(D2XXException("Device must be open to set data characteristics."))
+FT_SetDataCharacteristics(handle, DWORD(wordlength), DWORD(stopbits), DWORD(parity))
+end
+
+
+status(d::D2XXDevice) = status(fthandle(d))
+
+function status(handle::FT_HANDLE)
+  isopen(handle) || throw(D2XXException("Device must be open to check status."))
+  flags = FT_GetModemStatus(handle)
+  modemstatus = flags & 0xFF
+  linestatus = (flags >> 8) & 0xFF
+  mflaglist = Dict{String, Bool}()
+  lflaglist = Dict{String, Bool}()
+  mflaglist["CTS"]  = (modemstatus & 0x10) == 0x10
+  mflaglist["DSR"]  = (modemstatus & 0x20) == 0x20
+  mflaglist["RI"]   = (modemstatus & 0x40) == 0x40
+  mflaglist["DCD"]  = (modemstatus & 0x80) == 0x89
+  # Below is only non-zero for windows
+  lflaglist["OE"]   = (linestatus  & 0x02) == 0x02
+  lflaglist["PE"]   = (linestatus  & 0x04) == 0x04
+  lflaglist["FE"]   = (linestatus  & 0x08) == 0x08
+  lflaglist["BI"]   = (linestatus  & 0x10) == 0x10
+  mflaglist, lflaglist
+end
+
+
+"""
+    driverversion(d::D2XXDevice)
+
+Get the driver version for the D2XXDevice. Device must be open.
+"""
+driverversion(d::D2XXDevice) = driverversion(fthandle(d))
+
+"""
+    driverversion(handle::FT_HANDLE)
+
+Get the driver version for an open D2XX device `FT_HANDLE`.
+"""
+function driverversion(handle::FT_HANDLE)
+  isopen(handle) || throw(D2XXException("Device must be open to check driver version"))
+  version = FT_GetDriverVersion(handle)
+  @assert (version >> 24) & 0xFF == 0x00 # 4th byte should be 0 according to docs
+  patch = version & 0xFF
+  minor = (version >> 8) & 0xFF
+  major = (version >> 16) & 0xFF
+  VersionNumber(major,minor,patch)
+end
+
+
+Base.flush(d::D2XXDevice) = flush(fthandle(d))
+
+function Base.flush(handle::FT_HANDLE)
+  isopen(handle) || throw(D2XXException("Device must be open to flush."))
+  FT_StopInTask(handle)
+  FT_Purge(handle, FT_PURGE_RX|FT_PURGE_RX)
+  FT_RestartInTask(handle)
+end
+
+
+# Other Functions
+#
+function createdeviceinfolist()
+  numdevs = FT_CreateDeviceInfoList()
+end
+
+
+function getdeviceinfodetail(deviceidx)
+  0 <= deviceidx < createdeviceinfolist() || throw(D2XXException("Device index $deviceidx not in range."))
+  idx, flags, typ, id, locid, serialnumber, description, fthandle = FT_GetDeviceInfoDetail(deviceidx)
+end
+
+
+# Library Functions
+#
+function libversion()
+  version = FT_GetLibraryVersion()
+  @assert (version >> 24) & 0xFF == 0x00 # 4th byte should be 0 according to docs
+  patch = version & 0xFF
+  minor = (version >> 8) & 0xFF
+  major = (version >> 16) & 0xFF
+  VersionNumber(major,minor,patch)
+end
+
+
+# D2XXDevice Accessors
+#
 """
     deviceidx(d::D2XXDevice)
 
@@ -147,184 +389,5 @@ description(d::D2XXDevice) = d.description
 Get the D2XXDevice device D2XX handle of type ``::FT_HANDLE`.
 """
 fthandle(d::D2XXDevice) = d.fthandle
-
-function driverversion(handle::FT_HANDLE)
-  isopen(handle) || throw(D2XXException("Device must be open to check driver version"))
-  version = FT_GetDriverVersion(handle)
-  @assert (version >> 24) & 0xFF == 0x00 # 4th byte should be 0 according to docs
-  patch = version & 0xFF
-  minor = (version >> 8) & 0xFF
-  major = (version >> 16) & 0xFF
-  VersionNumber(major,minor,patch)
-end
-
-
-function libversion()
-  version = FT_GetLibraryVersion()
-  @assert (version >> 24) & 0xFF == 0x00 # 4th byte should be 0 according to docs
-  patch = version & 0xFF
-  minor = (version >> 8) & 0xFF
-  major = (version >> 16) & 0xFF
-  VersionNumber(major,minor,patch)
-end
-
-
-function status(handle::FT_HANDLE)
-  isopen(handle) || throw(D2XXException("Device must be open to check status."))
-  flags = FT_GetModemStatus(handle)
-  modemstatus = flags & 0xFF
-  linestatus = (flags >> 8) & 0xFF
-  mflaglist = Dict{String, Bool}()
-  lflaglist = Dict{String, Bool}()
-  mflaglist["CTS"]  = (modemstatus & 0x10) == 0x10
-  mflaglist["DSR"]  = (modemstatus & 0x20) == 0x20
-  mflaglist["RI"]   = (modemstatus & 0x40) == 0x40
-  mflaglist["DCD"]  = (modemstatus & 0x80) == 0x89
-  # Below is only non-zero for windows
-  lflaglist["OE"]   = (linestatus  & 0x02) == 0x02
-  lflaglist["PE"]   = (linestatus  & 0x04) == 0x04
-  lflaglist["FE"]   = (linestatus  & 0x08) == 0x08
-  lflaglist["BI"]   = (linestatus  & 0x10) == 0x10
-  mflaglist, lflaglist
-end
-
-
-"""
-    Base.close(handle::FT_HANDLE)
-
-Closes an open FTD2XX device and marks its handle as closed.
-"""
-function Base.close(handle::FT_HANDLE)
-  if isopen(handle)
-    FT_Close(handle)
-  end
-  return
-end
-
-
-function Base.readbytes!(handle::FT_HANDLE, b::AbstractVector{UInt8}, nb=length(b))
-  isopen(handle) || throw(D2XXException("Device must be open to read."))
-  nbav = bytesavailable(handle)
-  if nbav < nb
-    nb = nbav
-  end
-  if length(b) < nb
-    resize!(b, nb)
-  end
-  nbrx = FT_Read(handle, b, nb)
-end
-
-
-function Base.write(handle::FT_HANDLE, buffer::Vector{UInt8})
-  isopen(handle) || throw(D2XXException("Device must be open to write."))
-  FT_Write(handle, buffer, length(buffer))
-end
-
-
-function baudrate(handle::FT_HANDLE, baud)
-  isopen(handle) || throw(D2XXException("Device must be open to set baudrate."))
-  FT_SetBaudRate(handle, baud)
-end
-
-
-function datacharacteristics(handle::FT_HANDLE; 
-                             wordlength::FTWordLength = BITS_8, 
-                             stopbits::FTStopBits = STOP_BITS_1, 
-                             parity::FTParity = PARITY_NONE)
-  isopen(handle) || throw(D2XXException("Device must be open to set data characteristics."))
-  FT_SetDataCharacteristics(handle, wordlength, stopbits, parity)
-end
-
-
-function Compat.bytesavailable(handle::FT_HANDLE)
-  isopen(handle) || throw(D2XXException("Device must be open to check bytes available."))
-  FT_GetQueueStatus(handle)
-end
-
-
-Base.eof(handle::FT_HANDLE) = (bytesavailable(handle) == 0)
-
-
-function Base.readavailable(handle::FT_HANDLE)
-  b = @compat Vector{UInt8}(undef, bytesavailable(handle))
-  readbytes!(handle, b)
-  b
-end
-
-
-"""
-    open(str::AbstractString, openby::FTOpenBy)
-
-Open an FTD2XX device.
-
-# Arguments
- - `str::AbstractString` : Device identifier. Type depends on `openby`
- - `openby::FTOpenBy` : Indicator of device identifier `str` type.
-
-# Example
-
-```julia-repl
-julia> numdevs = FT_CreateDeviceInfoList()
-0x00000004
-
-julia> idx, flags, type, id, locid, serialnumber, description, fthandle = FT_GetDeviceInfoDetail(0)
-(0, 0x00000002, 0x00000007, 0x04036011, 0x00000000, "FT3AD2HCD", "USB <-> Serial Converter D", FT_HANDLE(Ptr{Nothing} @0x0000000000000000))
-
-julia> handle = open(description, OPEN_BY_DESCRIPTION)
-FT_HANDLE(Ptr{Nothing} @0x0000000000dfe740)
-
-julia> isopen(handle)
-true
-
-julia> close(handle)
-
-julia> handle = open(serialnumber, OPEN_BY_SERIAL_NUMBER)
-FT_HANDLE(Ptr{Nothing} @0x0000000005448ea0)
-
-julia> isopen(handle)
-true
-
-julia> close(handle)
-```
-"""
-Base.open(str::AbstractString, openby::FTOpenBy) = FT_OpenEx(str, DWORD(openby))
-
-
-function Base.flush(handle::FT_HANDLE)
-  isopen(handle) || throw(D2XXException("Device must be open to flush."))
-  FT_StopInTask(handle)
-  FT_Purge(handle, FT_PURGE_RX|FT_PURGE_RX)
-  FT_RestartInTask(handle)
-end
-
-
-function Base.isopen(handle::FT_HANDLE)
-  open = true
-  if ptr(handle) == C_NULL
-    open = false
-  else
-    try
-      FT_GetModemStatus(handle)
-    catch ex
-      if ex == FT_INVALID_HANDLE
-        open = false
-      else
-        rethrow(ex)
-      end
-    end
-  end
-  open
-end
-
-
-function createdeviceinfolist()
-  numdevs = FT_CreateDeviceInfoList()
-end
-
-
-function getdeviceinfodetail(deviceidx)
-  0 <= deviceidx < createdeviceinfolist() || throw(D2XXException("Device index $deviceidx not in range."))
-  idx, flags, typ, id, locid, serialnumber, description, fthandle = FT_GetDeviceInfoDetail(deviceidx)
-end
 
 end
--- test/hardware/LibFTD2XX.jl
+++ test/hardware/LibFTD2XX.jl
@@ -29,7 +29,7 @@ using Test
     @test fthand isa FT_HANDLE
   end
   idx, flgs, typ, devid, locid, serialn, descr, fthand = LibFTD2XX.getdeviceinfodetail(0)
-  @info "high level: testing device $description"
+  @info "high level: testing device $descr"
 
   # FT_HANDLE functions...
   @testset "FT_HANDLE" begin
@@ -76,12 +76,35 @@ using Test
     @test nwr == length(txbuf)
     @test txbuf == ones(UInt8, 10)
 
+    # flush and eof
+    retval = flush(handle)
+    @test eof(handle)
+    @test retval == nothing
+    @test isopen(handle)
+
     # driverversion 
     ver = driverversion(handle)
     @test ver isa VersionNumber
     @test_throws D2XXException driverversion(FT_HANDLE())
 
-    # close 
+    # datacharacteristics
+    retval = datacharacteristics(handle, wordlength = BITS_8, stopbits = STOP_BITS_1, parity = PARITY_NONE)
+    @test retval == nothing
+
+    # status
+    mflaglist, lflaglist = status(handle)
+    @test mflaglist isa Dict{String, Bool}
+    @test lflaglist isa Dict{String, Bool}
+    @test haskey(mflaglist, "CTS")
+    @test haskey(mflaglist, "DSR")
+    @test haskey(mflaglist, "RI")
+    @test haskey(mflaglist, "DCD")
+    @test haskey(lflaglist, "OE")
+    @test haskey(lflaglist, "PE")
+    @test haskey(lflaglist, "FE")
+    @test haskey(lflaglist, "BI")
+
+    # close and isopen
     retval = close(handle)
     @test retval == nothing
     @test !isopen(handle)
@@ -118,6 +141,78 @@ using Test
     @test length(devices) == numdevs
     @test all(deviceidx(devices[d]) == deviceidx(D2XXDevice(d-1)) for d = 1:numdevs)
 
+    # isopen
+    @test all(.!isopen.(devices))
+
+    # open
+    retval = open.(devices)
+    @test all(retval .== nothing)
+    @test all(isopen.(devices))
+  
+    # bytesavailable
+    nbs = bytesavailable.(devices)
+    @test all(nbs .>= 0)
+
+    device = devices[1] # choose device 1...
+    nb = nbs[1]
+
+    # read
+    rxbuf = read(device, nb)
+    @test length(rxbuf) == nb
+
+    # write
+    txbuf = ones(UInt8, 10)
+    nwr = write(device, txbuf)
+    @test nwr == length(txbuf)
+    @test txbuf == ones(UInt8, 10)
+
+    # readavailable
+    rxbuf = readavailable(device)
+    @test rxbuf isa AbstractVector{UInt8}
+
+    # baudrate
+    retval = baudrate(device, 9600)
+    @test retval == nothing
+    txbuf = ones(UInt8, 10)
+    nwr = write(device, txbuf)
+    @test nwr == length(txbuf)
+    @test txbuf == ones(UInt8, 10)
+
+    # flush and eof
+    retval = flush(device)
+    @test eof(device)
+    @test retval == nothing
+    @test isopen(device)
+
+    # driverversion 
+    ver = driverversion(device)
+    @test ver isa VersionNumber
+    @test_throws D2XXException driverversion(FT_HANDLE())
+
+    # datacharacteristics
+    retval = datacharacteristics(device, wordlength = BITS_8, stopbits = STOP_BITS_1, parity = PARITY_NONE)
+    @test retval == nothing
+
+    # status
+    mflaglist, lflaglist = status(device)
+    @test mflaglist isa Dict{String, Bool}
+    @test lflaglist isa Dict{String, Bool}
+    @test haskey(mflaglist, "CTS")
+    @test haskey(mflaglist, "DSR")
+    @test haskey(mflaglist, "RI")
+    @test haskey(mflaglist, "DCD")
+    @test haskey(lflaglist, "OE")
+    @test haskey(lflaglist, "PE")
+    @test haskey(lflaglist, "FE")
+    @test haskey(lflaglist, "BI")
+
+    # close and isopen (all devices)
+    retval = close.(devices)
+    @test all(retval .== nothing)
+    @test all(.!isopen.(devices))
+    @test all(LibFTD2XX.Wrapper.ptr.(fthandle.(devices)) .== C_NULL)
+    close.(devices) # check can close more than once without issue...
+    @test all(.!isopen.(devices))
 
   end
 
