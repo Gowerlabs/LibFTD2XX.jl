ad8fcfcc5330a3b9e64a0a3daa94f2ce8170e517
Reuben Hill <ReubenHill@users.noreply.github.com>
2019-03-21 11:48:59 +0000

    update timeout comment

--- src/LibFTD2XX.jl
+++ src/LibFTD2XX.jl
@@ -440,6 +440,10 @@ timeouts(fthandle(d) , timeout_rd, timeout_wr)
 
 Set the timeouts of an open [`FT_HANDLE`](@ref) using [`FT_SetTimeouts`](@ref).
 
+Behaviour is undefined for `timeout_rd` = 0 and `timeout_wr` = 0. 
+`timeout_rd` = 0 appears to cause [`read`](@ref) and [`readavailable`](@ref) to 
+block.
+
 See also: [`isopen`](@ref), [`open`](@ref)
 """
 function timeouts(handle::FT_HANDLE, timeout_rd, timeout_wr)
