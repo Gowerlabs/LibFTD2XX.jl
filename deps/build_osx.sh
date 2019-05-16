b8f7b334bb77f9ab8d68230b40c92a4381b4db69
Samuel Powell <samuelpowell@users.noreply.github.com>
2019-05-16 13:18:13 +0100

    Support MacOS

--- deps/build_osx.sh
+++ deps/build_osx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Mount the drivers
-MOUNT_POINT=$(hdiutil attach -noautoopen "./downloads/D2XX1.4.4.dmg" | egrep 'Volumes' | grep -o "/Volumes/.*")
+MOUNT_POINT=$(hdiutil attach -noautoopen "./usr/downloads/D2XX1.4.4.dmg" | egrep 'Volumes' | grep -o "/Volumes/.*")
 
 # Copy driver contents to lib directory
 `cp -r "$MOUNT_POINT/D2XX/" "./usr/lib"`
