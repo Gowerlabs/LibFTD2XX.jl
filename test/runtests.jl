d1fcb9c09ac195f62b1a494d2acd30cd19c7fadb
Samuel Powell <samuelpowell@users.noreply.github.com>
2020-02-15 11:51:01 +0000

    WIP

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,12 +9,12 @@ using LibFTD2XX
 include("util.jl")
 
 #numdevs = LibFTD2XX.createdeviceinfolist()
-if numdevs > 0
-  @info "found $numdevs devices. Running hardware tests..."
-  include("hardware/wrapper.jl")
-  include("hardware/LibFTD2XX.jl")
-else
-  @info "found $numdevs devices. Running nohardware tests..."
+# if numdevs > 0
+#   @info "found $numdevs devices. Running hardware tests..."
+#   include("hardware/wrapper.jl")
+#   include("hardware/LibFTD2XX.jl")
+# else
+# @info "found $numdevs devices. Running nohardware tests..."
   # include("nohardware/wrapper.jl")
   #include("nohardware/LibFTD2XX.jl")
 end
